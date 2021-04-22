a4712c68185a39092542e6cb9d77ad901590ddd1
liushui <978995652@qq.com>
2021-04-22 21:44:05 +0800

    更新1

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
+        System.out.println("更新1");
     }
 }
